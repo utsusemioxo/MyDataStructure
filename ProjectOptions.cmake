73602f0ca8b408e3b7b2e7c374914846511fc2fd
Template Janitor <template.janitor@example.com>
2023-11-26 13:58:55 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,7 +4,7 @@ include(CMakeDependentOption)
 include(CheckCXXCompilerFlag)
 
 
-macro(myproject_supports_sanitizers)
+macro(MyDataStructure_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
     set(SUPPORTS_UBSAN ON)
   else()
@@ -18,183 +18,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(MyDataStructure_setup_options)
+  option(MyDataStructure_ENABLE_HARDENING "Enable hardening" ON)
+  option(MyDataStructure_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    MyDataStructure_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    MyDataStructure_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  MyDataStructure_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR MyDataStructure_PACKAGING_MAINTAINER_MODE)
+    option(MyDataStructure_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(MyDataStructure_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(MyDataStructure_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(MyDataStructure_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(MyDataStructure_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(MyDataStructure_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(MyDataStructure_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(MyDataStructure_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(MyDataStructure_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(MyDataStructure_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(MyDataStructure_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(MyDataStructure_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(MyDataStructure_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(MyDataStructure_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(MyDataStructure_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(MyDataStructure_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(MyDataStructure_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(MyDataStructure_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(MyDataStructure_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(MyDataStructure_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(MyDataStructure_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(MyDataStructure_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(MyDataStructure_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(MyDataStructure_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(MyDataStructure_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(MyDataStructure_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      MyDataStructure_ENABLE_IPO
+      MyDataStructure_WARNINGS_AS_ERRORS
+      MyDataStructure_ENABLE_USER_LINKER
+      MyDataStructure_ENABLE_SANITIZER_ADDRESS
+      MyDataStructure_ENABLE_SANITIZER_LEAK
+      MyDataStructure_ENABLE_SANITIZER_UNDEFINED
+      MyDataStructure_ENABLE_SANITIZER_THREAD
+      MyDataStructure_ENABLE_SANITIZER_MEMORY
+      MyDataStructure_ENABLE_UNITY_BUILD
+      MyDataStructure_ENABLE_CLANG_TIDY
+      MyDataStructure_ENABLE_CPPCHECK
+      MyDataStructure_ENABLE_COVERAGE
+      MyDataStructure_ENABLE_PCH
+      MyDataStructure_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  MyDataStructure_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (MyDataStructure_ENABLE_SANITIZER_ADDRESS OR MyDataStructure_ENABLE_SANITIZER_THREAD OR MyDataStructure_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(MyDataStructure_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(MyDataStructure_global_options)
+  if(MyDataStructure_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    MyDataStructure_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  MyDataStructure_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(MyDataStructure_ENABLE_HARDENING AND MyDataStructure_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR MyDataStructure_ENABLE_SANITIZER_UNDEFINED
+       OR MyDataStructure_ENABLE_SANITIZER_ADDRESS
+       OR MyDataStructure_ENABLE_SANITIZER_THREAD
+       OR MyDataStructure_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${MyDataStructure_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${MyDataStructure_ENABLE_SANITIZER_UNDEFINED}")
+    MyDataStructure_enable_hardening(MyDataStructure_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(MyDataStructure_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(MyDataStructure_warnings INTERFACE)
+  add_library(MyDataStructure_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  MyDataStructure_set_project_warnings(
+    MyDataStructure_warnings
+    ${MyDataStructure_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(MyDataStructure_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    configure_linker(myproject_options)
+    configure_linker(MyDataStructure_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  MyDataStructure_enable_sanitizers(
+    MyDataStructure_options
+    ${MyDataStructure_ENABLE_SANITIZER_ADDRESS}
+    ${MyDataStructure_ENABLE_SANITIZER_LEAK}
+    ${MyDataStructure_ENABLE_SANITIZER_UNDEFINED}
+    ${MyDataStructure_ENABLE_SANITIZER_THREAD}
+    ${MyDataStructure_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(MyDataStructure_options PROPERTIES UNITY_BUILD ${MyDataStructure_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(MyDataStructure_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      MyDataStructure_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(MyDataStructure_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    MyDataStructure_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(MyDataStructure_ENABLE_CLANG_TIDY)
+    MyDataStructure_enable_clang_tidy(MyDataStructure_options ${MyDataStructure_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(MyDataStructure_ENABLE_CPPCHECK)
+    MyDataStructure_enable_cppcheck(${MyDataStructure_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(MyDataStructure_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    MyDataStructure_enable_coverage(MyDataStructure_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(MyDataStructure_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(MyDataStructure_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(MyDataStructure_ENABLE_HARDENING AND NOT MyDataStructure_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR MyDataStructure_ENABLE_SANITIZER_UNDEFINED
+       OR MyDataStructure_ENABLE_SANITIZER_ADDRESS
+       OR MyDataStructure_ENABLE_SANITIZER_THREAD
+       OR MyDataStructure_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    MyDataStructure_enable_hardening(MyDataStructure_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
